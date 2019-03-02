27ed1675bff4f9938e3a9289e4d1722557a99e8f
Lauri Elias <lauri.elias@indoorsman.ee>
2019-03-02 20:00:23 +0200

    Add mandate method parsing

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ import bs4
 import requests
 
 district_url_template = 'http://rk2015.vvk.ee/detailed-%s.html'
-elected_members_url = 'http://rk2015.vvk.ee/elected-members.html'
+mandate_method_url = 'http://rk2015.vvk.ee/acquired-mandates.html'
 
-elected_people = []
-elected_members_response = requests.get(elected_members_url)
-for elected_party_row in bs4.BeautifulSoup(elected_members_response.text, 'html.parser') \
-        .select('.elected-party > tbody > tr'):
-    for person_data in elected_party_row.select('td'):
-        person_data_contents = person_data.contents[0]
-        if person_data_contents != '\xa0':
-            elected_people.append(person_data_contents)
+mandate_methods_response = requests.get(mandate_method_url)
+parsed_mandate_methods_response = bs4.BeautifulSoup(mandate_methods_response.text, 'html.parser')
+methods_to_candidates_map = {}
+for i, mandate_type_table in enumerate(parsed_mandate_methods_response.select('table.mandates')):
+    if i == 0:
+        mandate_type = 'personal'
+    elif i == 1:
+        mandate_type = 'district'
+    else:
+        mandate_type = 'compensation'
+    methods_to_candidates_map[mandate_type] = []
+    for candidate_row in mandate_type_table.select('tr')[1:]:
+        candidate_name = candidate_row.select('td')[6].contents[0]
+        methods_to_candidates_map[mandate_type].append(candidate_name)
 
 results_file = codecs.open('results_rk2015.txt', 'w', 'utf-8')
-results_file.write('number\tname\tdistrict\tparty\tvotes\tgot_mandate')
+results_file.write('number\tname\tdistrict\tparty\tvotes\tmandate_method')
 results_file.write('\n')
 for i in range(1, 13):
     response = requests.get(district_url_template % i)
@@ -31,8 +37,16 @@ for i in range(1, 13):
             candidate_registration_number = candidate_data.select('td')[1].contents[0]
             candidate_name = candidate_data.select('td')[2].contents[0]
             candidate_total_votes = candidate_data.select('td > strong')[0].contents[0]
-            candidate_got_mandate = candidate_name in elected_people
+            candidate_mandate_method = None
+            if candidate_name in methods_to_candidates_map['personal']:
+                candidate_mandate_method = 'personal'
+            elif candidate_name in methods_to_candidates_map['district']:
+                candidate_mandate_method = 'district'
+            elif candidate_name in methods_to_candidates_map['compensation']:
+                candidate_mandate_method = 'compensation'
+            else:
+                candidate_mandate_method = None
             results_file.write('\t'.join(
                 [candidate_registration_number, candidate_name, str(i), current_party, candidate_total_votes,
-                 str(candidate_got_mandate), '\n']))
+                 str(candidate_mandate_method), '\n']))
 results_file.close()
